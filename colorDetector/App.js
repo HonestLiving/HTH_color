00a73bcc422e493623e7a294d8fcfccc7d69a672
TheAwakenedBob <107313261+TheAwakenedBob@users.noreply.github.com>
2023-03-04 18:21:14 -0500

    Camera
    Working Camera

--- colorDetector/App.js
+++ colorDetector/App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect, useRef} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  const[hasCameraPermission, setHasCameraPermission] = useState(null);
+  const[image, setImage] = useState(null);
+  const[type, setType] = useState(Camera.Constants.Type.back);
+  const[flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null); 
+
+  //asks for camera and library permissions
+  useEffect(() => {
+    (async ()=> {
+    //library permission
+    MediaLibrary.requestPermissionsAsync();
+    //camera permission
+    const cameraStatus = await Camera.requestCameraPermissionsAsync(); 
+    setHasCameraPermission(cameraStatus.status === 'granted'); 
+    })();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Test!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style = {styles.camera}
+        type = {type}
+        flashMode = {flash}
+        //references the camera here
+        ref = {cameraRef}
+        >
+          <Text> Hello </Text>
+        </Camera>
     </View>
   );
 }
@@ -14,7 +40,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+    borderRadius: 20,
+  }
 });
